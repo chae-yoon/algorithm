e138ea11188cd129a4432e64725e625bf10ff365
chae-yoon <75927175+chae-yoon@users.noreply.github.com>
2023-01-18 11:44:37 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,16 +1,7 @@
-S = input('')
+word = input()
+answer = []
 
-dic = {}
+for c in range(97, 123):
+    answer.append(word.find(chr(c)))
 
-for i in range(97, 123):
-    dic[i] = -1
-
-for char in S:
-    if dic[ord(char)] < 0:
-        dic[ord(char)] = S.index(char)
-    else:
-        continue
-
-lis = map(str, list(dic.values()))
-
-print(' '.join(lis))
+print(*answer)
