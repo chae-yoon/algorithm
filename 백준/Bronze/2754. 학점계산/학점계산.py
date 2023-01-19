ee609987182c75f20fccdb948155b189b2e67aff
chae-yoon <75927175+chae-yoon@users.noreply.github.com>
2023-01-19 14:02:04 +0900

    [Bronze V] Title: 학점계산, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2754. 학점계산/학점계산.py
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -1,17 +1,14 @@
-score = {
-    'A+' : 4.3, 
-    'A0' : 4.0,
-    'A-' : 3.7,
-    'B+' : 3.3,
-    'B0' : 3.0,
-    'B-' : 2.7,
-    'C+' : 2.3,
-    'C0' : 2.0,
-    'C-' : 1.7,
-    'D+' : 1.3,
-    'D0' : 1.0,
-    'D-' : 0.7,
+c_score = {
+    'A' : 4.0,
+    'B' : 3.0,
+    'C' : 2.0,
+    'D' : 1.0,
     'F' : 0.0,
+    '+' : 0.3,
+    '-' : -0.3,
 }
 
-print(score[input()])
+scores = input()
+result = [c_score.get(score, 0) for score in scores]
+
+print(sum(result))
